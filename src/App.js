c9b50f7548ccadae591cc6bda6183d27e860d150
Raju 007 <rajubhashabathini007@gmail.com>
2024-04-12 21:36:25 +0530

    added mobile breakpoints

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
+import Card from "./pages/Card";
 
 function App() {
+  const [pageNum, setPageNum] = useState(1);
+  const [landData, setLandData] = useState([]);
+
+  const getMyData = async (pageNumProps, pageSize) => {
+    try {
+      const data = await fetch(
+        `https://prod-be.1acre.in/lands/?ordering=-updated_at&page=${pageNumProps}&page_size=${pageSize}`
+      );
+      const jsonData = await data.json();
+      const results = jsonData.results;
+      console.log("results :", results);
+      setLandData(landData.concat(results));
+    } catch (error) {
+      console.log("error is :", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("pageNum pageNum", pageNum);
+    getMyData(pageNum, 10);
+  }, [pageNum]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-10 flex justify-start">
+      <InfiniteScroll
+        className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-8"
+        dataLength={landData.length}
+        next={() => setPageNum((prev) => prev + 1)}
+        hasMore={true}
+        loader={<div className="min-h-60 w-full">Loading...</div>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {landData.map((ele) => {
+          return (
+            <div className="max-w-[372px] border border-red-400 rounded-lg">
+              <Card data={ele} />
+            </div>
+          );
+        })}{" "}
+      </InfiniteScroll>
     </div>
   );
 }
---
+++ src/pages/Card.js
@@ -0,0 +1,111 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const Card = ({ data }) => {
+  console.log("data 0000", data);
+  return (
+    <div key={data?.id} className="relative flex flex-col ">
+      <div
+        className="absolute w-full flex justify-between  top-4  gap-3 z-10
+        "
+      >
+        <div className="">
+          {data.tags.some((ele) => ele.tag_type === "highway_facing") ? (
+            <div className="bg-yellow-300  flex gap-2 rounded-r-full pr-2">
+              <img src="https://1acre.in/static/images/icons/chance-icon.svg" />
+              Highway
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="flex flex-row gap-3 ">
+          <img
+            className="bg-white rounded-full p-2 cursor-pointer"
+            src="https://1acre.in/static/images/icons/share.svg"
+            alt="share"
+          />
+          <img
+            className="bg-white rounded-full p-2 cursor-pointer"
+            src="https://1acre.in/static/images/icons/favorite.svg"
+            alt="wishlist"
+          />
+        </div>
+      </div>
+      <div className="carousel-part">
+        {data?.land_media.length > 0 ? (
+          <>
+            <Carousel controls={true} indicators={false} interval={null}>
+              {data?.land_media.map((element) => {
+                return (
+                  <Carousel.Item key={element?.image}>
+                    <img
+                      loading="lazy"
+                      className=" max-h-60 w-[100%] min-h-60 "
+                      src={element?.image}
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      {element.category === "approach_road" ? (
+                        <p className="flex flex-wrap justify-end items-end bg-black text-white">
+                          Approach Road
+                        </p>
+                      ) : element.category === "fencing" ? (
+                        <p className="flex justify-end items-end bg-black text-white">
+                          Fencing
+                        </p>
+                      ) : null}
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                );
+              })}
+            </Carousel>
+          </>
+        ) : (
+          <img className="max-h-60 w-[100%] min-h-60 " src="" alt="" />
+        )}
+      </div>
+      <div className="p-3 text-part ">
+        <div className="font-bold text-[12px] lg:text-[18px] md:text-[18px] flex  gap-3 items-center">
+          {data.village_name},{data.mandal_name}{" "}
+          <img src="https://1acre.in/static/images/icons/verified-active.svg" />
+        </div>
+        <div className="font-bold text-[12px] lg:text-[18px] md:text-[18px]">
+          {data.district_name}
+          {"(dt)"}
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          {data.total_land_size_in_acres.acres > 0.0 ? (
+            <>
+              <p className=" text-[12px] lg:text-[18px] md:text-[18px]  ">
+                {data.total_land_size_in_acres.acres} {""}
+                {"Acres ."} {""}
+              </p>
+              {"₹ "}
+              <p className=" text-[12px] lg:text-[18px] md:text-[18px]  ">
+                {data.price_per_acre / 100} crores per acre
+              </p>
+            </>
+          ) : (
+            <>
+              <p className="font-bold text-[12px] lg:text-[18px] md:text-[18px]">
+                {data.total_land_size_in_acres.guntas} {"Guntas ."} {"₹ "}{" "}
+              </p>
+              {data.total_price > 100 ? (
+                <p className=" font-normal text-[12px] lg:text-[18px] md:text-[18px]">
+                  {data.total_price / 100} crores for full property
+                </p>
+              ) : (
+                <p className="font-normal text-[12px] lg:text-[18px] md:text-[18px]">
+                  {data.total_price.toFixed(2)} lakhs for full property
+                </p>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
